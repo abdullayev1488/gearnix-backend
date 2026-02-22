8cb764c0181082b02a7d2068acc87a6f2ae9e6c0
Elman Abdullayev <elmanabdullayev001@gmail.com>
2026-02-23 02:46:31 +0400

    test pre-commit hook

--- controllers/category/create.category.js
+++ controllers/category/create.category.js
@@ -3,6 +3,7 @@ import { Category } from "../../models/category.model.js"
 export const createCategory = async (req, res) => {
     console.log(req.body);
     
+    
     try {
         const { name, image } = req.body;
         const category = await Category.create({ name, images: {shop:image} });
